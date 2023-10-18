790c819e6df5e6b8a5f195c1ce26a3a2db7c13a2
stevenfvg <stevenvalladaresgiron@gmail.com>
2023-10-17 23:36:26 -0300

    feat(script): :sparkles: powershell script to install web development tools

---
+++ InstallWebDevTools.ps1
@@ -0,0 +1,16 @@
+# Install Google Chrome Dev
+winget install Google.Chrome.Dev --accept-source-agreements --disable-interactivity
+
+# Install Firefox Developer Edition
+winget install Mozilla.Firefox.DeveloperEdition --accept-source-agreements --disable-interactivity
+
+# Install VSCode
+winget install Microsoft.VisualStudioCode --accept-source-agreements --disable-interactivity
+
+# Install Postman
+winget install Postman.Postman --accept-source-agreements --disable-interactivity
+
+# Install pgAdmin 4
+winget install PostgreSQL.pgAdmin --accept-source-agreements --disable-interactivity
+
+Write-Host "Installation process completed. The applications have been installed successfully."
